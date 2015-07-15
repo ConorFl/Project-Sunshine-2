a916e21b429a74a779b2416ad2120f44b0f8b1cc
ConorFl <conoreflanagan@gmail.com>
2015-07-14 23:32:43 -0700

    add array adapter with dummy data

--- app/src/main/java/com/example/android/sunshine2/MainActivityFragment.java
+++ app/src/main/java/com/example/android/sunshine2/MainActivityFragment.java
@@ -5,6 +5,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
@@ -18,6 +24,33 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        String[] forecastArray = {
+                "Today - Sunny",
+                "Tomorrow - Sunny",
+                "Wednesday - Snow",
+                "Thursday - Sleet",
+                "Friday - Sunny",
+                "Saturday - Sunny",
+                "Sunday - Cloudy"
+        };
+
+        List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
+
+        ArrayAdapter<String> mForecastAdapter = new ArrayAdapter<String>(
+                // current context
+                getActivity(),
+                // id of list item layout
+                R.layout.list_item_view,
+                // id of textview to populate
+                R.id.list_item_forecast_textview,
+                // data
+                weekForecast);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(mForecastAdapter);
+
+        return rootView;
     }
 }

7b3e477fdf1cb270cbca41d5f385917fd37949d3
ConorFl <conoreflanagan@gmail.com>
2015-07-23 22:14:11 -0700

    add click listener with toast, add details activity (blank activity w/ fragment)

--- app/src/main/java/com/example/android/sunshine2/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine2/ForecastFragment.java
@@ -12,8 +12,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -95,6 +97,14 @@ public class ForecastFragment extends Fragment {
 
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String data = mForecastAdapter.getItem(position);
+                Toast.makeText(getActivity(), data, Toast.LENGTH_LONG).show();
+            }
+        });
 
         return rootView;
     }

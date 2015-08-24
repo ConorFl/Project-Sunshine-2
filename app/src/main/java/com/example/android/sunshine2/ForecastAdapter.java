5bb9850239bb180b366359a85f582f9097301b91
ConorFl <conoreflanagan@gmail.com>
2015-08-23 21:08:19 -0700

    start using TODAY view in list but update newView in ForecastAdapter to choose the view depending on if its the firs cursor result.

--- app/src/main/java/com/example/android/sunshine2/ForecastAdapter.java
+++ app/src/main/java/com/example/android/sunshine2/ForecastAdapter.java
@@ -16,14 +16,33 @@ import com.example.android.sunshine2.data.WeatherContract;
  * from a {@link android.database.Cursor} to a {@link android.widget.ListView}.
  */
 public class ForecastAdapter extends CursorAdapter {
+    private final int VIEW_TYPE_TODAY = 0;
+    private final int VIEW_TYPE_FUTURE_DAY = 1;
 
     public ForecastAdapter(Context context, Cursor c, int flags) {
         super(context, c, flags);
     }
 
     @Override
+    public int getItemViewType(int position) {
+        return (position == 0) ? VIEW_TYPE_TODAY : VIEW_TYPE_FUTURE_DAY;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 2;
+    }
+
+    @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        View view = LayoutInflater.from(context).inflate(R.layout.list_item_forecast, parent, false);
+        int viewType = getItemViewType(cursor.getPosition());
+        int layoutId = -1;
+        if (viewType == VIEW_TYPE_TODAY) {
+            layoutId = R.layout.list_item_forecast_today;
+        } else if (viewType == VIEW_TYPE_FUTURE_DAY) {
+            layoutId = R.layout.list_item_forecast;
+        }
+        View view = LayoutInflater.from(context).inflate(layoutId, parent, false);
 
         return view;
     }
